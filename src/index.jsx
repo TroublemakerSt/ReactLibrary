300266d3dbd80b4ce14e5e5983d03a4e387c3f27
Ilya Kulikov <ilya-art@mail.ru>
2017-03-25 13:34:41 +0200

    Simple routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
 import App from './App';
+import About from './components/About';
+import Books from './components/Books';
+import NotFound from './components/NotFound';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="about" component={About} />
+    <Route path="books" component={Books} />
+    <Route path="*" component={NotFound} />
+  </Router>,
+document.getElementById('root'));
